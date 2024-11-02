6b51bae6f63ec15d4228b205b07223bf27d5d68d
anthonyb8 <anthonybaxter819@gmail.com>
2024-11-02 11:11:32 -0400

    ADD: Added methods for mbn.LiveData.

--- midasClient/trading.py
+++ midasClient/trading.py
@@ -1,7 +1,7 @@
 import requests
 from typing import Dict
 from .utils import load_url
-from mbn import BacktestData
+from mbn import BacktestData, LiveData
 
 
 class TradingClient:
@@ -13,10 +13,39 @@ class TradingClient:
 
     # self.api_key = api_key
 
+    def create_live(self, data: LiveData):
+        url = f"{self.api_url}/live/create"
+
+        response = requests.post(url, json=data.__dict__())
+
+        if response.status_code != 200:
+            raise ValueError(f"Create live failed: {response.text}")
+        return response.json()
+
+    def delete_live(self, id: int) -> Dict:
+        url = f"{self.api_url}/live/delete"
+
+        response = requests.delete(url, json=id)
+
+        if response.status_code != 200:
+            raise ValueError(f"Deleting live failed: {response.text}")
+        return response.json()
+
+    def get_live(self, id: int) -> Dict:
+        url = f"{self.api_url}/live/get?id={id}"
+
+        response = requests.get(url)  # json=id)
+
+        if response.status_code != 200:
+            raise ValueError(
+                f"Live instance retrieval failed: {response.text}"
+            )
+        return response.json()
+
     def create_backtest(self, data: BacktestData):
         url = f"{self.api_url}/backtest/create"
 
-        response = requests.post(url, json=data)
+        response = requests.post(url, json=data.__dict__())
 
         if response.status_code != 200:
             raise ValueError(
